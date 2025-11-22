0c6aef7127e40a860c05f213602cfdfc40b595ec
knowyx <knowyx@gmail.com>
2025-11-23 00:42:38 +0300

    update func main started to write main func

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("foo")
+	//defining variables
+	var usrName = os.Getenv("USER")
+	var ext string
+	var interval int
+	var dir = "/home/" + usrName + "/Desktop"
+
+	//print greeting
+	fmt.Printf("Привет, %s!\nЭто go-project, написанный just for fun\n", usrName)
+	fmt.Println("Это программа будет создавать файлы на рабочем столе с заданым расширением и интервалом.\n" +
+		"Введите расширение (без точки) и интервал")
+	_, err := fmt.Scan(&ext, &interval)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Создаю файлы '.%s' в дикертории '%s' каждые %d секунд\n", ext, dir, interval)
+
+	//main loop
+
 }
